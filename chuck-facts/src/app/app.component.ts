1559f0399c6557100d3be7f33f8a04e3855e17c0
Dmytro Prysiazhnyi <dimpris@gmail.com>
2020-11-11 22:55:40 +0200

    Removed http API service

--- chuck-facts/src/app/app.component.ts
+++ chuck-facts/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +7,18 @@ import { ApiService } from './services/api.service';
 })
 export class AppComponent {
   title = 'chuck-facts';
-  private api: ApiService;
-  private categories: string[] = [];
-  private isRandom = true;
-  private selectedCategory: string;
-  private username = '';
-  private answers: any[] = [];
+  categories: string[] = [];
+  isRandom = true;
+  selectedCategory: string;
+  username = '';
+  answers: any[] = [];
 
-  constructor(apiService: ApiService) {
-    this.api = apiService;
+  constructor() {
+    
   }
 
   ngOnInit() {
-    this.api.GetCategories().subscribe((data: string[]) => {
-      this.categories = data;
-    });
+    
   }
 
   public RandomClicked() {
@@ -34,30 +30,8 @@ export class AppComponent {
   }
 
   public Search() {
-    if (this.isRandom) {
-      this.api.SearchRandom(this.selectedCategory, this.username).subscribe((data: any) => {
-        this.setAnswer([data]);
-      });
-    } else {
-      this.api.Search(this.username).subscribe((data: any) => {
-        this.setAnswer(data.result);
-      });
-    }
+    
   }
 
-  public selectChangeHandler(e: any) {
-    if (e.target.value) {
-      this.selectedCategory = e.target.value;
-    }
-  }
-
-  public inputChangeHandler(e: any) {
-    if (e.target.value) {
-      this.username = e.target.value;
-    }
-  }
-
-  private setAnswer(data: any[]) {
-    this.answers = data;
-  }
+  
 }
